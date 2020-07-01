f8d7b73b1b8ecaad6178075a0917c44ed64a9d86
Sabin <sansiven@gmail.com>
2020-07-01 13:43:51 +0545

    Finished adding slider as required

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,21 @@
 $(document).ready(function(){
     let addSlider = {
 
+        // well if in the time of working I dont have access to their site but a way to add JS file then, 
+        // I would initialise slick slider here and then add the functionlaity.
+
+        addSlickSlider: function(){
+            $('.sb-slider').slick({
+                autoplay: true,
+                arrows: true,
+                dots: true,
+
+            })
+        },
+
         init: function(){
-            console.log('working')
+            var obj =this;
+            obj.addSlickSlider()
         }
 
     }.init();
